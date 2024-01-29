20600e815c7a3678138f98c6811406f9a40efd3b
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-01-29 16:54:49 -0500

    Revert "feat: Notify update: requirements"
    This reverts commit 75af89e5cd5a6f2584c20391974bad7ad23bd662.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from configs.configs import obtener_url_desde_config
 from api.request import make_request
 from player.voice import reconocer_voz
 from player.sound import reproducir_audio
-from plyer import notification
 
 # Función para interactuar con el chatbot a través de API
 def api_chatbot(texto: str, url: str, name:str) -> None:
@@ -10,26 +9,12 @@ def api_chatbot(texto: str, url: str, name:str) -> None:
         file_audio = make_request(message=texto, url= url)
         reproducir_audio(file_audio)
 
-# Notificar lo que detecto
-def notificacion(titulo:str, mensaje:str):
-    notification.notify(
-        title=titulo,
-        message=mensaje,
-        app_name='Bot',  
-        timeout=10  
-    )
-
 if __name__ == "__main__":
     config_set = obtener_url_desde_config()
     while True:
         try:
-            voz = reconocer_voz()
-            notificacion(
-                titulo='ChatBot',
-                mensaje=voz
-            )
             api_chatbot(
-                texto= voz, 
+                texto= reconocer_voz(), 
                 url= config_set["url"], 
                 name= config_set["name"]
             )
