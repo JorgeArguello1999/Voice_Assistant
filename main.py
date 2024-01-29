4152308366daf8e3ca9357c0a8c68f3b4db0f008
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-01-29 12:37:11 -0500

    feat: Its functional

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import requests 
-import os
-
 from configs.configs import obtener_url_desde_config
+from api.request import make_request
 from player.voice import reconocer_voz
 from player.sound import reproducir_audio
 
 # Función para interactuar con el chatbot a través de API
 def api_chatbot(texto: str, url: str, name:str) -> None:
     if texto != False and name in texto:
-        pass
+        file_audio = make_request(message=texto, url= url)
+        reproducir_audio(file_audio)
 
 if __name__ == "__main__":
     config_set = obtener_url_desde_config()
